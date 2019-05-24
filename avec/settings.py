bd653ecab6768fad0819504d7c7e666ab3a6db86
Paulo Mann <paulomann@id.uff.br>
2019-05-24 17:49:52 -0300

    Revert "Código inicial para carregar os dados do OpenFace."
    This reverts commit 44dac3017abbaccf45c551c6b9c20d9dd5c94954.

--- avec/settings.py
+++
@@ -1,7 +0,0 @@
-from os.path import dirname, join
-
-ROOT = join(dirname(dirname(__file__)))
-
-PATH_TO_DEVELOPMENT_DATA = join(ROOT, "data", "validation")
-PATH_TO_TRAINING_DATA = join(ROOT, "data", "train")
-PATH_TO_TEST_DATA = join(ROOT, "data", "test")
